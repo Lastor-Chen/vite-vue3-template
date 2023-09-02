73735d193fde530ca07200bde3051566d884c041
Lastor <lastoraya@gmail.com>
2023-09-02 18:43:15 +0800

    [修改] Sidebar 同步當前 route path

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,17 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
   <aside>
-    <ElMenu class="h-full" default-active="dashboard" router>
-      <ElMenuItem index="dashboard">
+    <ElMenu class="h-full" :default-active="route.path" router>
+      <ElMenuItem index="/dashboard">
         <span>Dashboard</span>
       </ElMenuItem>
-      <ElMenuItem index="ad-format">
+      <ElMenuItem index="/ad-format">
         <span>Ad Format</span>
       </ElMenuItem>
-      <ElMenuItem index="page-b">
+      <ElMenuItem index="/page-b">
         <span>Navigator Two</span>
       </ElMenuItem>
     </ElMenu>
