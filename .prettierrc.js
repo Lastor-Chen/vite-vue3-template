194bf14306ebcbdae4ec542ec8551a240ff54741
Lastor <lastoraya@gmail.com>
2023-09-02 14:21:15 +0800

    [調整] prettier 配置

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  $schema: "https://json.schemastore.org/prettierrc",
+  singleQuote: true, // 單引號優先
+  semi: false, // 尾部分號
+  trailingComma: 'es5', // Object, Array 末段加逗號
+  tabWidth: 2,
+  bracketSameLine: false, // jsx html ">" 是否不斷行
+  printWidth: 120, // 自動斷行長度
+}
